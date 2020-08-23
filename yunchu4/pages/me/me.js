65ba57e4bfbddc2024d0d555dba964e3e8abd14e
文甲东 <wenjiadong@xiaomi.com>
2020-08-23 17:15:34 +0800

    1

--- yunchu4/pages/me/me.js
+++ yunchu4/pages/me/me.js
@@ -3,5 +3,11 @@ const bottomLogo = "../../img/yunchu.gif";
 Page({
   data:{
     bottomLogo
+  },
+  gotoPage: function(e){
+    console.log("GoTo:", e)
+    wx.navigateTo({
+      url: e.currentTarget.dataset.goto,
+    })
   }
 })
