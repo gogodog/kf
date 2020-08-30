bfb42d1077e6b9be99ee1638ee014889f8f2dac6
文甲东 <wenjiadong@xiaomi.com>
2020-08-30 19:00:59 +0800

    1

--- yunchu4/api/config.js
+++ yunchu4/api/config.js
@@ -1,7 +1,9 @@
 const config={//方便切换部署环境
-    api_url:"http://www.cwillow.com",
+    api_url:"http://127.0.0.1",
     api_key:"key111",
-    socket:"ws://www.cwillow.com"
+    socket:"ws://127.0.0.1/yapi/chat/"
 }
+
+
 
 export {config}
