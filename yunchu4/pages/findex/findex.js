5ce85476ce5991c1dd060c26e3db3c60fd4f4653
文甲东 <wenjiadong@xiaomi.com>
2020-09-09 22:48:38 +0800

    1

--- yunchu4/pages/findex/findex.js
+++ yunchu4/pages/findex/findex.js
@@ -3,14 +3,5 @@ Page({
   data: {
     StatusBar: appInst.globalData.StatusBar,
     CustomBar: appInst.globalData.CustomBar,
-  },
-  onGetUserInfo: function (e) {
-    console.log(e)
-    if (!this.logged && e.detail.userInfo) {
-      appInst.globalData.userInfo = e.detail.userInfo;
-      wx.switchTab({
-        url: '/pages/basics/home/home',
-      })
-    }
   }
 })
